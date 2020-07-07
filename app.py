3ad829b041c42ce63bafe4a34ab627ba9e669150
hendrik-mischo <hendrik.mischo@outlook.com>
2020-07-07 10:51:48 +0200

    update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
+import pickle
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 #from flask import Flask, request, jsonify
